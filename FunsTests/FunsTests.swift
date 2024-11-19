915eeeb234eb3f9e164810364213e4871bdd3ef2
thomasgriffin <ltgriffin01@gmail.com>
2024-11-19 07:42:12 -0500

    Initial Commit

---
+++ FunsTests/FunsTests.swift
@@ -0,0 +1,17 @@
+//
+//  FunsTests.swift
+//  FunsTests
+//
+//  Created by Thomas Griffin on 11/19/24.
+//
+
+import Testing
+@testable import Funs
+
+struct FunsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
